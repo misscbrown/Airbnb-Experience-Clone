455ef991a104730b322ddd4c029d98917817d0f6
cee breezy <djmisscbrown@hotmail.com>
2022-05-31 21:33:48 +0100

    added navbar functionality

---
+++ airbnb-app/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+import airbnbLogo from "../assets/airbnb-logo.png"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={airbnbLogo} className="nav--logo" />
+        </nav>
+    )
+}
+
